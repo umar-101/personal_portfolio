cb971a6f2a510a9a7477f0ecd5cb0a26d45be449
UMAR <umarmushtaq101@gmail.com>
2022-05-22 14:43:04 +0500

    file-structure and navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react'
+import { HiMenuAlt4, HiX } from 'react-icons/hi';
+import { motion } from 'framer-motion'
+
+import './Navbar.scss'
+
+import { images } from '../../constants'
+const Navbar = () => {
+
+    const [toggle, setToggle] = useState(false)
+    return (
+        <nav className='app__navbar'>
+            <div className='app__navbar-logo'>
+                <img src={images.logo} alt="logo" />
+            </div>
+
+            <ul className='app__navbar-links'>
+                {['home', 'about', 'work', ' skills', 'contact'].map((item) => (
+                    <li className='app__flex p-text' key={`link-${item}`}>
+                        <div>
+                            <a href={`#${item}`}>{item}</a>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+
+
+            < div className='app_navbar-menu'>
+                <HiMenuAlt4 onClick={() => setToggle(true)} />
+
+                {toggle && (
+                    <motion.div
+                        whileInView={{ x: [300, 0] }}
+                        transition={{ duration: 0.85, ease: 'easeOut' }}
+                    >
+
+                        <HiX onClick={() => setToggle(false)}>
+                            {['home', 'about', 'work', ' skills', 'contact'].map((item) => (
+                                <li key={item}>
+                                    <div>
+                                        <a href={`#${item}`} onClick={() => setToggle(false)}>{item}</a>
+                                    </div>
+                                </li>
+                            ))}
+                        </HiX>
+                    </motion.div>
+
+                )
+                }
+
+
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
